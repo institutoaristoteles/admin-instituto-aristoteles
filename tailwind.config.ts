4062dc48d2b4e417d4f4691598c313e7345dcaf9
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-10-21 02:04:54 -0300

    ajusta content do tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,18 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './shared/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
     },
     extend: {
       fontFamily: {
-        sans: "var(--font-inter)",
+        sans: 'var(--font-inter)',
       },
     },
   },
