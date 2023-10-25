c294e0fa24729f2536677b41f0d9665258c84f4f
Yasmin Santana <103133345+yassantana@users.noreply.github.com>
2023-10-25 14:16:42 -0300

    Cria pagina de login

---
+++ app/login/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from "primereact/button";
+import { InputText,  } from "primereact/inputtext";
+import { Password } from "primereact/password";
+
+export default function LoginPage(){
+    return (
+        <div className="flex flex-col gap-2 justify-start items-start">
+            <h1>Login</h1>
+            <div className="flex flex-col gap-1">
+            <label htmlFor="nome">Nome</label>
+            <InputText type="text" id="nome"/>
+            </div>
+            <div className="flex flex-col gap-1">
+            <label htmlFor="password">Senha</label>
+            <Password type="password" id="password"/>
+            </div>
+            
+            <Button label="Enviar"/>
+
+            
+        </div>
+
+    )
+}
