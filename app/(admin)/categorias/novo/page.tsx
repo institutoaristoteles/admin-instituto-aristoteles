aa02fc8ebb91c08d8006104887665656925a28e0
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-11-02 22:53:36 -0300

    adiciona validação ao formulário de criação de categorias

--- app/(admin)/categorias/novo/page.tsx
+++ app/(admin)/categorias/novo/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import React from 'react'
-import { InputText } from 'primereact/inputtext'
-import { Button } from 'primereact/button'
+import CategoryForm from '@/app/(admin)/categorias/novo/category-form'
 
 export default function NewCategoryPage() {
   return (
@@ -14,17 +13,7 @@ export default function NewCategoryPage() {
           </h2>
         </header>
 
-        <form className="max-w-prose flex flex-col items-start gap-5">
-          <label className="text-sm font-bold flex flex-col gap-1 w-full">
-            Nome
-            <InputText type="text" name="name" required />
-          </label>
-
-          <div className="flex items-center gap-2">
-            <Button label="Salvar" type="submit" />
-            <Button label="Cancelar" outlined type="reset" />
-          </div>
-        </form>
+        <CategoryForm />
       </div>
     </main>
   )
