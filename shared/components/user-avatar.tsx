33271f0ad15735565a7d617494f252e73fa34741
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-11-03 03:50:13 -0300

    ajusta avatar do usuário

--- shared/components/user-avatar.tsx
+++ shared/components/user-avatar.tsx
@@ -1,18 +1,16 @@
+'use client'
+
 import { Avatar } from 'primereact/avatar'
 import LogoutButton from '@/shared/components/logout-button'
 import React from 'react'
-import { getUser } from '@/shared/services/auth.service'
+import { useAuth } from '@/shared/contexts/auth-provider'
 
-export default async function UserAvatar() {
-  const user = await getUser()
-
-  if (!user) {
-    return <></>
-  }
+export default function UserAvatar() {
+  const { user } = useAuth()
 
   return (
     <div className="flex items-start gap-2 ml-auto">
-      <Avatar image={user.avatar} label={user.name[0]} />
+      <Avatar image={user.avatar} label={user.name[0]} size="normal" />
 
       <div className="flex flex-col items-start">
         <span>{user.name}</span>
