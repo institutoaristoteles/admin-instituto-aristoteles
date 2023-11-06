f3d419476b97162cee674d20a34958e4188b3fac
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-11-06 00:31:34 -0300

    cria componentes para inputs complexos

--- app/(admin)/usuarios/users-form.tsx
+++ app/(admin)/usuarios/users-form.tsx
@@ -1,27 +1,23 @@
 'use client'
 
+import TemporaryPasswordField from '@/shared/components/temporary-password-field'
+import UserRolesField from '@/shared/components/user-roles-field'
 import { UserRoles } from '@/shared/models/user-profile'
 import { SaveUser } from '@/shared/services/users.service'
 import { zodResolver } from '@hookform/resolvers/zod'
 import clsx from 'clsx'
-import { generate } from 'generate-password'
 import Link from 'next/link'
 import { PrimeIcons } from 'primereact/api'
 import { Button } from 'primereact/button'
 import { InputText } from 'primereact/inputtext'
-import { Password } from 'primereact/password'
-import { RadioButton } from 'primereact/radiobutton'
-import React, { useCallback } from 'react'
-import { useForm } from 'react-hook-form'
-import toast from 'react-hot-toast'
-import { useCopyToClipboard } from 'usehooks-ts'
+import React from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-const PASSWORD_LENGTH = 42
+const PASSWORD_LENGTH = 8
 const USERNAME_MIN_LENGTH = 5
 const USERNAME_PATTERN = /^(?=[a-zA-Z0-9._]{5,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/
 
-const roleOptions = Object.entries(UserRoles)
 const roleKeys = Object.keys(UserRoles) as [string, ...string[]]
 
 const saveUserSchema = z.object({
@@ -33,165 +29,101 @@ const saveUserSchema = z.object({
     )
     .regex(USERNAME_PATTERN, 'Formato incorreto de usuário'),
   email: z.string().email('Informe um endereço de e-mail válido'),
-  password: z.string({ coerce: true }).min(PASSWORD_LENGTH),
+  password: z
+    .string({ coerce: true })
+    .min(PASSWORD_LENGTH, 'Este campo é obrigatório'),
   role: z.enum(roleKeys),
 })
 
 export default function UsersForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setValue,
-    watch,
-  } = useForm<SaveUser>({
+  const methods = useForm<SaveUser>({
     resolver: zodResolver(saveUserSchema),
     shouldFocusError: false,
     reValidateMode: 'onChange',
     defaultValues: { role: 'editor' },
   })
 
-  const currentPassword = watch('password', '')
-  const selectedRole = watch('role')
-
-  const [, copy] = useCopyToClipboard()
-
-  const generatePass = useCallback(async () => {
-    const generatedPassword = generate({
-      length: PASSWORD_LENGTH,
-      numbers: true,
-      strict: true,
-    })
-
-    setValue('password', generatedPassword)
-  }, [setValue])
-
-  const copyPassword = useCallback(async () => {
-    await copy(currentPassword)
-    toast.success('Senha copiada')
-  }, [copy, currentPassword])
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods
 
   const onSubmit = async (values: SaveUser) => {
     console.log(values)
   }
 
   return (
-    <form
-      className="max-w-prose flex flex-col items-start gap-5"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <label className="text-sm font-bold flex flex-col gap-1 w-full">
-        E-mail
-        <InputText
-          {...register('email')}
-          type="email"
-          className={clsx({ 'p-invalid': errors.email })}
-          autoFocus
-        />
-        {errors.email && (
-          <span className="text-[#ff7b7b] font-normal">
-            {errors.email.message}
-          </span>
-        )}
-      </label>
-
-      <label className="text-sm font-bold flex flex-col gap-1 w-full">
-        Usuário
-        <InputText
-          {...register('username')}
-          className={clsx({ 'p-invalid': errors.username })}
-        />
-        {errors.username && (
-          <span className="text-[#ff7b7b] font-normal">
-            {errors.username.message}
-          </span>
-        )}
-      </label>
-
-      <div className="flex flex-col items-start gap-1 w-full">
-        <label
-          className="text-sm font-bold flex flex-col items-start gap-1 w-full"
-          htmlFor="password"
-        >
-          Senha temporária
-        </label>
-        <div className="p-inputgroup flex-1">
-          <Password
-            readOnly
-            inputId="password"
-            value={currentPassword}
-            feedback={false}
-            toggleMask
-            {...register('password')}
-            className={clsx({ 'p-invalid': errors.password })}
+    <FormProvider {...methods}>
+      <form
+        className="max-w-prose flex flex-col items-start gap-5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label className="text-sm font-bold flex flex-col gap-1 w-full">
+          E-mail
+          <InputText
+            {...register('email')}
+            type="email"
+            className={clsx({ 'p-invalid': errors.email })}
+            autoFocus
           />
-          <Button
-            severity="info"
-            icon={PrimeIcons.COPY}
-            type="button"
-            disabled={!currentPassword.length}
-            onClick={copyPassword}
-          />
-        </div>
-        <Button
-          severity="info"
-          icon={PrimeIcons.KEY}
-          label="Gerar nova senha"
-          type="button"
-          onClick={generatePass}
-          link
-          className="px-0"
-        />
-
-        {errors.password && (
-          <span className="text-[#ff7b7b] font-normal">
-            {errors.password.message}
-          </span>
-        )}
-      </div>
-
-      <div className="flex flex-col items-start gap-1 w-full">
-        <label className="text-sm font-bold flex flex-col items-start gap-1 w-full">
-          Perfil
+          {methods.formState.errors.email && (
+            <span className="text-[#ff7b7b] font-normal">
+              {methods.formState.errors.email.message}
+            </span>
+          )}
         </label>
-        {errors.role && (
-          <span className="text-[#ff7b7b] font-normal">
-            {errors.role.message}
-          </span>
-        )}
 
-        <div className="flex flex-col gap-5 py-5">
-          {roleOptions.map(([value, role]) => (
-            <label key={value} className="flex items-center gap-2">
-              <RadioButton
-                {...register('role')}
-                value={value}
-                checked={value === selectedRole}
-                onChange={(event) => setValue('role', event.value)}
-              />
-              {role}
-            </label>
-          ))}
+        <label className="text-sm font-bold flex flex-col gap-1 w-full">
+          Usuário
+          <InputText
+            {...register('username')}
+            className={clsx({ 'p-invalid': errors.username })}
+          />
+          {errors.username && (
+            <span className="text-[#ff7b7b] font-normal">
+              {errors.username.message}
+            </span>
+          )}
+        </label>
+
+        <div className="flex flex-col items-start gap-1 w-full">
+          <label
+            className="text-sm font-bold flex flex-col items-start gap-1 w-full"
+            htmlFor="password"
+          >
+            Senha temporária
+          </label>
+
+          <TemporaryPasswordField />
         </div>
-      </div>
 
-      <div className="flex flex-col justify-start md:flex-row gap-2 w-full">
-        <Button
-          label="Salvar"
-          type="submit"
-          className="w-full md:w-auto"
-          icon={PrimeIcons.SAVE}
-        />
-        <Link href="/usuarios">
+        <div className="flex flex-col items-start gap-1 w-full">
+          <label className="text-sm font-bold flex flex-col items-start gap-1 w-full">
+            Perfil
+          </label>
+
+          <UserRolesField />
+        </div>
+
+        <div className="flex flex-col justify-start md:flex-row gap-2 w-full">
           <Button
-            label="Cancelar"
-            outlined
-            type="button"
+            label="Salvar"
+            type="submit"
             className="w-full md:w-auto"
-            icon={PrimeIcons.CHEVRON_LEFT}
+            icon={PrimeIcons.SAVE}
           />
-        </Link>
-      </div>
-    </form>
+          <Link href="/usuarios">
+            <Button
+              label="Cancelar"
+              outlined
+              type="button"
+              className="w-full md:w-auto"
+              icon={PrimeIcons.CHEVRON_LEFT}
+            />
+          </Link>
+        </div>
+      </form>
+    </FormProvider>
   )
 }
