2ae6bebbe204089ffd1c1b92a9080b25de0b167e
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-11-20 04:14:32 -0300

    ajusta formulário

--- app/(admin)/usuarios/novo/success-user-dialog.tsx
+++ app/(admin)/usuarios/novo/success-user-dialog.tsx
@@ -1,79 +1,49 @@
-import CopyButton from '@/shared/components/copy-button'
 import { SaveUser } from '@/shared/services/users.service'
 import { PrimeIcons } from 'primereact/api'
 import { Button } from 'primereact/button'
 import { Dialog } from 'primereact/dialog'
-import { Message } from 'primereact/message'
-import { Panel } from 'primereact/panel'
-import { Password } from 'primereact/password'
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 
 export interface SuccessModalProps {
   user?: SaveUser
   onClose: () => void
 }
 
-function SuccessUserModal(props: SuccessModalProps) {
-  const [canClose, setCanClose] = useState(false)
-
-  const onPasswordCopy = useCallback(() => {
-    setCanClose(true)
-  }, [])
-
+function SuccessUserModal({ onClose, user }: SuccessModalProps) {
   const footer = useMemo(
     () => (
       <div className="pt-5">
         <Button
-          label="Fechar"
+          label="Entendido"
           icon={PrimeIcons.CHECK}
           className="m-0 max-md:w-full"
-          disabled={!canClose}
-          onClick={props.onClose}
+          onClick={onClose}
+          severity="success"
         />
       </div>
     ),
-    [canClose, props.onClose],
+    [onClose],
   )
 
   return (
     <Dialog
-      onHide={props.onClose}
-      header="Usuário criado com sucesso!"
-      visible={!!props.user}
+      onHide={onClose}
+      visible={!!user}
       draggable={false}
       closable={false}
-      className="min-w-[30%]"
+      className="min-w-[20%]"
       footer={footer}
     >
-      <Message
-        severity="info"
-        text="Salve a senha provisória e informe ao usuário."
-        className="mb-5 w-full justify-start"
-      />
-
-      <div className="flex flex-col md:grid md:grid-cols-7 gap-5">
-        <div className="md:col-span-full">
-          <Panel header="Usuário">{props.user?.username}</Panel>
-        </div>
-
-        <div className="md:col-span-full">
-          <Panel header="Senha provisória">
-            <div className="flex flex-col gap-2 items-start">
-              <Password
-                value={props.user?.password}
-                toggleMask
-                feedback={false}
-                inputClassName="border-none p-0 w-full"
-                className="w-full"
-              />
-            </div>
-          </Panel>
-          <CopyButton
-            value={props.user?.password}
-            onCopy={onPasswordCopy}
-            label="Copiar senha provisória"
-            className="px-0"
-          />
+      <div className="flex flex-col text-center gap-5 items-center">
+        <i className={`${PrimeIcons.CHECK_CIRCLE} text-6xl text-[#86EFAC]`} />
+        <div className="flex flex-col gap-2">
+          <h2 className="text-[#86EFAC] text-2xl font-bold">
+            Usuário criado com sucesso!
+          </h2>
+          <p>
+            Um e-mail foi enviado para o e-mail <span>{user?.email}</span> com
+            as informações de acesso.
+          </p>
         </div>
       </div>
     </Dialog>
