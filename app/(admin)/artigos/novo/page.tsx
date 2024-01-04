296f1ee000d3b35c846cc2ef170b24c7553516c6
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2024-01-04 01:50:48 -0300

    adiciona links

--- app/(admin)/artigos/novo/page.tsx
+++ app/(admin)/artigos/novo/page.tsx
@@ -1,10 +1,22 @@
 import PostForm from '@/app/(admin)/artigos/post-form'
+import PageHeader from '@/shared/components/page-header'
 import React from 'react'
 
 export default function NewPostPage() {
+  const breadcrumbs = [
+    {
+      label: 'Artigos',
+      path: '/artigos',
+    },
+    {
+      label: 'Novo',
+    },
+  ]
+
   return (
     <main>
       <div className="container">
+        <PageHeader title="Novo artigo" breadcrumbs={breadcrumbs} />
         <PostForm />
       </div>
     </main>
