77501df972f682ef693eafd25937a27ef3032625
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2024-01-06 02:54:54 -0300

    add image field

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,19 @@
 import type { Config } from 'tailwindcss'
 
+const colors = {
+  primary: 'var(--primary-color)',
+  'text-color': 'var(--text-color)',
+  'text-color-secondary': 'var(--text-color-secondary)',
+  'surface-card': 'var(--surface-card)',
+  'surface-border': 'var(--surface-border)',
+  'surface-overlay': 'var(--surface-overlay)',
+  'surface-a': 'var(--surface-a)',
+  'surface-b': 'var(--surface-b)',
+  'surface-c': 'var(--surface-c)',
+  'surface-d': 'var(--surface-d)',
+  'primary-color-text': 'var(--primary-color-text)',
+}
+
 const config: Config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,19 +24,7 @@ const config: Config = {
       center: true,
       padding: '1rem',
     },
-    colors: {
-      primary: 'var(--primary-color)',
-      'text-color': 'var(--text-color)',
-      'text-color-secondary': 'var(--text-color-secondary)',
-      'surface-card': 'var(--surface-card)',
-      'surface-border': 'var(--surface-border)',
-      'surface-overlay': 'var(--surface-overlay)',
-      'surface-a': 'var(--surface-a)',
-      'surface-b': 'var(--surface-b)',
-      'surface-c': 'var(--surface-c)',
-      'surface-d': 'var(--surface-d)',
-      'primary-color-text': 'var(--primary-color-text)',
-    },
+    colors,
     extend: {
       fontFamily: {
         sans: 'var(--font-inter)',
